f9c22761673f88836c8a55ee023dd352a51cb1af
Amit Dagar <amitdagar4096@gmail.com>
2023-12-08 12:07:37 +0530

    new version check added

--- dist/Button.js
+++ dist/Button.js
@@ -4,10 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var Button = function Button(_ref) {
   var text = _ref.text,
     onClick = _ref.onClick;
-  return /*#__PURE__*/React.createElement("button", {
+  return /*#__PURE__*/_react["default"].createElement("button", {
     "class": "btn btn-ghost-active",
     onClick: onClick
   }, text);
--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Button = ({ text, onClick }) => {
   return (
     <button class="btn btn-ghost-active" onClick={onClick}>
